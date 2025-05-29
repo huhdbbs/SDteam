9feeea79d7a775a865675f8ebf274541f66c5b6b
huhdbbs <dsvedgbrssfergeg@gmail.com>
2025-05-29 13:56:15 +0200

    KingLegacy.lua

---
+++ KingLegacy.lua
@@ -0,0 +1,21 @@
+-- Chargement Fluent UI
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+local Window = Fluent:CreateWindow({
+    Title = "King Legacy",
+    SubTitle = "SD Team",
+    TabWidth = 100,
+    Size = UDim2.fromOffset(480, 300),
+    Acrylic = false,
+    Theme = "Darker",
+    MinimizeKey = Enum.KeyCode.RightControl
+})
+
+
+-- Fluent provides Lucide Icons, they are optional
+local Tabs = {
+    Info = Window:AddTab({ Title = "Info", Icon = "info" }),
+    Main = Window:AddTab({ Title = "Main", Icon = "circle-ellipsis" }),
+    Sea = Window:AddTab({ Title = "Sea", Icon = "waves" }),
+    Settings = Window:AddTab({ Title = "Misc", Icon = "sliders-horizontal" })
+}
